c4779eac1cfc90560c3d96d9b826bc8effa48092
root <root@ubuntu-512mb-sgp1-01.localdomain>
2017-05-29 15:33:52 +0000

    evaluated all of txt file

--- spell_checker/spell_checker.py
+++ spell_checker/spell_checker.py
@@ -16,33 +16,47 @@ tool = language_check.LanguageTool('en-US')
 #MORFOLOGIK_RULE_EN_US
 
 FIXDIR = './fix/'
+ExceptRuleId = ['COMMA_PARENTHESIS_WHITESPACE','WHITESPACE_RULE', 'MORFOLOGIK_RULE_EN_US', 'EN_QUOTES', 'EN_UNPAIRED_BRACKETS']
+def mkdirp(DIRPATH) :
+    if not os.path.isdir(DIRPATH) :
+	os.makedirs(DIRPATH)
 
 def dirCheck(DIRPATH) :
     dirList = os.listdir(DIRPATH)
     for inFile in dirList :
         if(os.path.isdir(DIRPATH+inFile)) :
+	    print(DIRPATH+inFile+'/')
             dirCheck(DIRPATH+inFile+'/')
         elif(is_binary(DIRPATH+inFile)) :
             continue
         else :
             nLine = 0
             f = open(DIRPATH+inFile,'r')
-            fw = open(FIXDIR+inFile+'.fix','w')
+	    TEMPDIR = FIXDIR[:-1]+DIRPATH
+	    mkdirp(TEMPDIR)
+            fw = open(TEMPDIR+inFile+'.fix','w')
             while True:
                 line = f.readline()
                 if not line: break
-                matches = tool.check(line.decode('utf-8'))
-                for idx in range(len(matches)) :
-                    print str(matches[idx]), matches[idx].ruleId
+                #matches = tool.check(line.decode('utf-8'))
+		                
+                matches = tool.check(line)
+		for idx in range(len(matches)) :
+		    if matches[idx].ruleId in ExceptRuleId :
+			continue
+                    print DIRPATH+inFile, matches[idx].ruleId
                     data = str(nLine)+' | '+str(matches[idx].fromx)+' '+str(matches[idx].tox)+' | '+line[matches[idx].fromx:matches[idx].tox]+' | '+matches[idx].msg+'\n'
                     print data
-                    fw.write(str(matches[idx]).encode("utf8")+'\n')
-                    fw.write(data.encode("utf8")+'\n')
+                    #fw.write(str(matches[idx]).encode("utf8")+'\n')
+		    fw.write(str(matches[idx])+'\n')
+		    fw.write(data+'\n')
+                   # fw.write(data.encode("utf8")+'\n')
                 nLine += 1
             fw.close()
             f.close()
 
-DIR = './testdir/'
+#DIR = './testdir/'
+DIR = '/root/workspace/caffe2/'
 print DIR
 dirCheck(DIR)
 
